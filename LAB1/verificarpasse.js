40e7bd58fda6721424035755053198065708bb54
Sérgio Gonçalves <127428465+ISergioMiguelI@users.noreply.github.com>
2024-03-11 15:50:15 +0000

    Update verificarpasse.js

--- LAB1/verificarpasse.js
+++ LAB1/verificarpasse.js
@@ -1,9 +1,12 @@
+
 function verificaspasse() {
-        var password = document.getElementById("password").value;
+        
+    var password = document.getElementById("password").value;
         var retypePassword = document.getElementById("retypePassword").value;
 
+        
         if (password != retypePassword) {
-            alert("As senhas não correspondem. Por favor, tente novamente.");
+        alert("As senhas não correspondem. Por favor, tente novamente.");
             return false;
         }
 
