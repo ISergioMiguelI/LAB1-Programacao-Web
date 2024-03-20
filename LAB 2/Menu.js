36373815594e820d3a7ba055e8f41ac09f1413ad
Sérgio Gonçalves <127428465+ISergioMiguelI@users.noreply.github.com>
2024-03-20 23:47:09 +0000

    Tasks
    Need to add the tasks that i creat to the table

--- LAB 2/Menu.js
+++ LAB 2/Menu.js
@@ -4,18 +4,13 @@ document.getElementById('nova-tarefa-form').addEventListener('submit', function(
   event.preventDefault();
 
   // Obtém os valores dos campos de entrada do formulário
-  var nome = document.getElementById('tarefa-nome').value;
   var descricao = document.getElementById('tarefa-descricao').value;
   var data = document.getElementById('tarefa-data').value;
 
   // Cria um novo elemento de linha para a tabela
   var row = document.createElement('tr');
 
-  // Cria e configura os elementos de célula para a linha
-  var nomeCell = document.createElement('td');
-  nomeCell.textContent = nome;
-  row.appendChild(nomeCell);
-
+  // Cria e configura as células para a nova linha da tabela
   var descricaoCell = document.createElement('td');
   descricaoCell.textContent = descricao;
   row.appendChild(descricaoCell);
@@ -24,28 +19,17 @@ document.getElementById('nova-tarefa-form').addEventListener('submit', function(
   dataCell.textContent = data;
   row.appendChild(dataCell);
 
-  // Adiciona a linha à tabela no container "Task Manager"
+  // Cria a célula de checkbox para a nova linha da tabela
+  var checkboxCell = document.createElement('td');
+  var checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.id = 'tarefa-feita';
+  checkboxCell.appendChild(checkbox);
+  row.appendChild(checkboxCell);
+
+  // Adiciona a nova linha à tabela no container "Task Manager"
   document.getElementById('taskList').appendChild(row);
 
   // Limpa os campos de entrada do formulário
   this.reset();
-
-  // Fecha o modal
-  var modalNovaTarefa = bootstrap.Modal.getInstance(document.getElementById('modal-nova-tarefa'));
-  modalNovaTarefa.hide();
-});
-// Adiciona um evento de clique a cada linha da tabela de tarefas
-var taskRows = document.querySelectorAll('#taskList tr');
-taskRows.forEach(function(row) {
-    row.addEventListener('click', function() {
-        // Encontra a descrição da tarefa na linha clicada
-        var descricao = this.cells[1].textContent;
-
-        // Preenche o campo de descrição no modal de edição
-        document.getElementById('tarefa-nome-editada').value = descricao;
-
-        // Abre o modal de edição
-        var modalEditarTarefa = new bootstrap.Modal(document.getElementById('modal-editar-tarefa'));
-        modalEditarTarefa.show();
-    });
 });
